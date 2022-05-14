588b585c2eb1161f1d3d00299ae44e11707b79e6
Ishan Agrawal <agrawalia@rknec.edu>
2022-05-14 10:08:17 +0530

    react frontend setup

--- src/dragon-app/src/App.js
+++ src/dragon-app/src/App.js
@@ -1,13 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Dapp</title>
-    <script src="index.js"></script>
-  </head>
-  <body>
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
     <button onclick="getName()">Name</button>
     <textarea id="name_textarea"></textarea><br />
     <button onclick="getSymbol()">Symbol</button>
@@ -25,5 +21,8 @@
       src="https://cdn.ethers.io/lib/ethers-5.2.umd.min.js"
       type="application/javascript"
     ></script>
-  </body>
-</html>
+    </div>
+  );
+}
+
+export default App;
